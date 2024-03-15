72ce13ec0b3d800ce4133078b3e0cf8ef8d8447c
aryangupta1105 <husltegamingfanclub@gmail.com>
2024-03-15 23:28:38 +0530

    Solved homework question

--- arrays/Arrays3/Arrays3HW/WavePrintMatrix/wavePrintmatrix.java
+++ arrays/Arrays3/Arrays3HW/WavePrintMatrix/wavePrintmatrix.java
@@ -4,11 +4,18 @@ import java.util.*;
 public class wavePrintmatrix{
     public static void main(String args[]){
         int[][] arr = {{1,2,3} , {4,5,6} , {7, 8, 9}};
+        int j = 0;
         for(int i = 0; i<arr.length; i++){
-            for(int j = 0; j<arr[i].length; j++){
-                System.out.print(arr[j][i] + " ");
+            if(j%2 == 0){
+                for(j = 0; j<arr[i].length; j++){
+                    System.out.print(arr[j][i] + " ");
+                }
             }
-            System.out.println();
+            else{
+                for(j = arr[i].length-1; j>=0; j--){
+                    System.out.print(arr[j][i] + " ");
+                }
+            }
         }
     }
 }
