628efc797c5303307071cbe0479a033a354b1980
aryangupta1105 <husltegamingfanclub@gmail.com>
2024-03-21 00:06:18 +0530

    find missing element by binary search

--- arrays/Searching/FindMissingElement.java
+++ arrays/Searching/FindMissingElement.java
@@ -7,16 +7,20 @@ public class FindMissingElement{
         int end = arr.length -1; 
         int mid = 0;
         while(start<end){
-            mid = start + (end - end)/2;
-            if((arr[mid] - arr[start])  == (mid - start)){
-                start = mid + 1;
-                if(mid == end) return mid+1;
+            mid = start + (end - start)/2;
+            if((arr[mid] - mid == 1)){
+                start = mid+1;
             }
-            else{
+            else if(arr[mid] - mid != 1){
                 end = mid;
             }
+            if(start == arr.length - 1 ) {
+                // If so, return the missing element
+                return arr[start] + 1;
+            }
+            if(arr[0] != 1) return 1;
         }
-        return mid;
+        return start+1;
         }
 
     static int findMissingElementBySum(int [] arr){
@@ -31,11 +35,11 @@ public class FindMissingElement{
         int missing = sum - sum2;
         return missing;
     }
-    
+
     public static void main(String args[]){
     // <---------------------FIND MISSING ELEMENT BY BINARY SEARCH------------------------>
 
-        int arr1[] = {1 , 2, 3 ,5, 6 ,7 , 8 , 9 , 10 , 11 , 12};
+        int arr1[] = {1,2,3,4,5,7,8 , 9};
         int ans = findMissingElement(arr1);
         System.out.println(ans);
 
