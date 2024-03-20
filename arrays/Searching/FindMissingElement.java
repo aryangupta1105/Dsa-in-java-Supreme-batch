0f1d2311d44b7e2364c2b9cbb34340c8c7f7c621
aryangupta1105 <husltegamingfanclub@gmail.com>
2024-03-20 22:26:07 +0530

    updated

--- arrays/Searching/FindMissingElement.java
+++ arrays/Searching/FindMissingElement.java
@@ -6,22 +6,43 @@ public class FindMissingElement{
         int start = 0; 
         int end = arr.length -1; 
         int mid = 0;
-        while(start <=end){
-            mid = start + (end -start)/2;
-            if(mid - start != arr[mid] - arr[start])
-            {
-                end = mid -1;
+        while(start<end){
+            mid = start + (end - end)/2;
+            if((arr[mid] - arr[start])  == (mid - start)){
+                start = mid + 1;
+                if(mid == end) return mid+1;
             }
             else{
-                start = mid + 1;
+                end = mid;
             }
         }
-        return start;
+        return mid;
+        }
+
+    static int findMissingElementBySum(int [] arr){
+        int n = arr.length + 1;
+        int sum = n* (n +1 )/2;
+        int sum2  = 0;
+        System.out.println(sum);
+        for(int i = 0; i<arr.length; i++){
+            sum2 += arr[i];
+        }
+        System.out.println(sum2);
+        int missing = sum - sum2;
+        return missing;
     }
+    
     public static void main(String args[]){
-        int arr1[] = {1 , 2, 3 ,4 ,5, 6 ,7 , 8 , 9 , 11 , 12 , 13};
+    // <---------------------FIND MISSING ELEMENT BY BINARY SEARCH------------------------>
+
+        int arr1[] = {1 , 2, 3 ,5, 6 ,7 , 8 , 9 , 10 , 11 , 12};
         int ans = findMissingElement(arr1);
         System.out.println(ans);
+
+    // <---------------FIND MISSING ELEMENT BY SUM--------------------------->
+        // int arr2[] = {1 , 2, 3 ,4 ,5, 6 ,7 , 8 , 9 , 10 , 11 , 13};
+        // int ans1 = findMissingElementBySum(arr2);
+        // System.out.println(ans1);
 
     }
 }
--- arrays/Searching/PeakElementMountainArray.java
+++ arrays/Searching/PeakElementMountainArray.java
@@ -2,7 +2,54 @@ import java.lang.*;
 import java.util.*;
 
 public class PeakElementMountainArray{
-    public static void main(String args[]){
-        
+    static int findingPeakElement(int arr[]){
+        int start = 0; 
+        int end = arr.length-1;
+        int mid = 0;
+        while(start<end){
+            mid = start + (end-start)/2;
+            if(arr[mid] < arr[mid+1])
+                start = mid+1;
+            else{
+                end = mid;
+            }
+        }
+        return arr[start];
     }
-}
+    static int findingPivotIndex(int arr[]){
+        int start = 0; 
+        int end = arr.length-1;
+        int mid = 0;
+        while(start<end){
+            mid = start + (end-start)/2;
+            if(arr[mid] > arr[end])
+                start = mid+1;
+            else{
+                end = mid;
+            }
+        }
+        return start;
+    }
+    static int SearchInRotatedArray(int arr[] , int key){
+        int start = 0; 
+        int end = arr.length-1;
+        int mid = 0;
+        while(start<end){
+            mid = start + (end-start)/2;
+            if(arr[mid] == key)
+                return mid;
+            if((key < arr[mid]) && (key>=arr[end]))
+                end = mid;
+            else start = mid+1;
+        }
+        return start;
+    }
+    public static void main(String args[]){
+        int arr[] = {0 , 8 , 15 , 3 , 2 , 1};
+        System.out.println("The peak element is : " + findingPeakElement(arr));
+        int arr2[] = {6, 7 , 8 , 1 ,2, 3 ,4 ,5};
+        System.out.println("The pivot element is at : " + findingPivotIndex(arr2));
+        int arr3[] = {6, 7 , 8 , 1 ,2, 3 ,4 ,5};
+        System.out.println("The peak element is at : " + SearchInRotatedArray(arr2 , 1));
+    }
+} 
